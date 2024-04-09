93cde73b000ac5b02084fe01fe575cb94fef20d5
M. Kozlik <malkak@skyvar.co.il>
2024-04-09 13:41:25 +0300

    add file1

--- main-project/main.tf
+++ main-project/main.tf
@@ -1,4 +1,22 @@
-# resource "local_file" "file1" {
-#   content = "hello"
-#   filename = "./file1"
-# }
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "NetworkWatcherRG"
+    storage_account_name = "myfirsttrail"
+    container_name       = "terraform-input"
+    key                  = "terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+  subscription_id = var.subscription_id
+}
+
+resource "local_file" "file1" {
+  content = "hello"
+  filename = "./file1"
+}
